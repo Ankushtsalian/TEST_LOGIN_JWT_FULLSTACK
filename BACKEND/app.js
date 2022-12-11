5845dbb96cb7165e27b596b0fdd6bf755b02f415
Ankush <ankushtsalian@gmail.com>
2022-12-11 18:25:25 +0530

    CONNECTED DB

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 require("express-async-errors");
-
+require("./db/connect");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const router = require("./routes/main");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +22,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
--- BACKEND/controllers/main.js
+++ BACKEND/controllers/main.js
@@ -1,13 +1,20 @@
-// login;
-// register;
-// dashboard;
+const jwt = require("jsonwebtoken");
+let token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsInVzZXJuYW1lIjoiIDJVcGRhdGUgVFNFVCIsImlhdCI6MTY3MDc2MjczNiwiZXhwIjoxNjczMzU0NzM2fQ.vv3YyAJCE_mPSvKeRdWauYRLvhP4tJLL-8C7eOvEGOY";
+const register = async (req, res) => {
+  const { username, password, resetPassword } = req.body;
+
+  const id = new Date().getDate();
+  token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status("200").json({ msg: { username, token } });
+};
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  res.status("200").json({ msg: { username, password } });
-};
-const register = async (req, res) => {
-  res.send("hello register");
+  res.status("200").json({ msg: { username, token } });
 };
 const dashboard = async (req, res) => {
   res.send("hello dashboard");
