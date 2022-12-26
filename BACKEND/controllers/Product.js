a507f65106251432e5b48e1cc7f3ce1ea3ef7342
Ankush <ankushtsalian@gmail.com>
2022-12-26 18:46:39 +0530

    ---------------file uploader create product controller incomplete-----------

--- BACKEND/controllers/Product.js
+++ BACKEND/controllers/Product.js
@@ -1,5 +1,18 @@
-const createProduct = (req, res) => {
-  res.send("Hello");
+const Product = require("../model/Product");
+const CustomAPIError = require("../errors/custom-error");
+
+const createProduct = async (req, res) => {
+  //   console.log(req.body);
+
+  try {
+    const product = await Product.create(req.body);
+    console.log(product);
+    res.status(200).json({ product });
+  } catch (error) {
+    throw new CustomAPIError(error, 400);
+  }
+
+  //   throw new CustomAPIError("Please provide email and password", 400);
 };
 
 const getAllProducts = (req, res) => {
