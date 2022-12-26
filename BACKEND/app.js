6962596fceca0483e148e069d3d960fedc4c6ef0
Ankush <ankushtsalian@gmail.com>
2022-12-26 18:26:50 +0530

    ---------------file uploader routes & controller-----------

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -13,13 +13,14 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const router = require("./routes/main");
 const jobRouter = require("./routes/JobRoute");
-
+const productRoutes = require("./routes/Product");
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/v1", router);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/products", productRoutes);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
