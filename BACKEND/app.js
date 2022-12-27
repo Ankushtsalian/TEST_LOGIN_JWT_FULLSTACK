1693e0abad8bffa1d30b74be80d46ade09425d8c
Ankush <ankushtsalian@gmail.com>
2022-12-27 15:13:08 +0530

    --------------CLOUDINARY SETUP FOR CLOUD----------------

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 require("express-async-errors");
 require("./db/connect");
+
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+
+  api_key: process.env.CLOUD_API_KEY,
+
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const connectDB = require("./db/connect");
 const express = require("express");
 const mongoose = require("mongoose");
